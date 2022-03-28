43c3b8d845c9a6d14e80450d668c6299a98f8c0e
Jing Tan <jtan1993@gmail.com>
2022-03-28 17:12:33 +0100

    ex 1

--- C#/C#2-Week1-Exercise1-Visual-Studio/Exercise1/Exercise1/Program.cs
+++ C#/C#2-Week1-Exercise1-Visual-Studio/Exercise1/Exercise1/Program.cs
@@ -18,8 +18,29 @@ namespace Exercise1
         /// <param name="args">command-line arguments</param>
         static void Main(string[] args)
         {
-
+            // Problem 1: Create objects: deck & 5 cards
+            Deck deck = new Deck();
+            Card[] cards = new Card[4];
+            
+            // Tell the deck to shuffle itself
+            deck.Shuffle();
+            
+            // Problem 2: Add card to the array(Take a card from the top of the deck and add it to element 0 in the array), flip it over, print it
+            cards[0] = deck.TakeTopCard();
+            cards[0].FlipOver();
+            cards[0].Print();
+            
+            // Problem 3: Add another card to the array, flip it over, print both cards
+            // Take a card from the top of the deck, add it to element 1 in the array.
+            // Flip the card at element 1 of the array over.
+            // Tell the cards at elements 0 & 1 of the array to print themselves
+            cards[1] = deck.TakeTopCard();
+            cards[1].FlipOver();
+            cards[1].Print();
+            cards[0].Print();
+            
             Console.WriteLine();
         }
+        
     }
 }
