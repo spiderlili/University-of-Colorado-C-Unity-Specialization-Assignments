733dd42c2d12c44b320f8c43f15b3e6595fb0f90
Jing Tan <jtan1993@gmail.com>
2022-03-29 20:14:54 +0100

    list refactor

--- C#/C#2-Week1-Exercise2-Visual-Studio/Exercise2/Exercise2/Program.cs
+++ C#/C#2-Week1-Exercise2-Visual-Studio/Exercise2/Exercise2/Program.cs
@@ -12,25 +12,26 @@ namespace Exercise2
     class Program
     {
         /// <summary>
-        /// Practice using lists
+        /// Practice using lists: convert the code to use a list of cards instead of an array
         /// </summary>
         /// <param name="args">command-line arguments</param>
         static void Main(string[] args)
         {
             // create deck and hand
             Deck deck = new Deck();
-            Card[] hand = new Card[5];
+            // Card[] hand = new Card[5]; 
+            List <Card> hand = new List<Card>();
 
             deck.Shuffle();
 
             // add card, flip over, and print
-            hand[0] = deck.TakeTopCard();
+            hand.Add(deck.TakeTopCard());
             hand[0].FlipOver();
             hand[0].Print();
             Console.WriteLine();
 
             // add another card, flip over, and print both cards
-            hand[1] = deck.TakeTopCard();
+            hand.Add(deck.TakeTopCard());
             hand[1].FlipOver();
             hand[1].Print();
             hand[0].Print();
